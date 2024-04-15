a4bdcd615ff90464d88f1d3180ab6e5723817dbc
juanregino <juanreginopenagos@gmail.com>
2024-04-15 13:56:51 -0500

    add file of constants

---
+++ src/constants/skills.js
@@ -0,0 +1,104 @@
+export const STACKSKILLS = [
+  {
+    name: "Java",
+    color: "#FAFAFA",
+    tecnology: "BackEnd",
+  },
+  {
+    name: "Mysql",
+    color: "#004c61",
+    tecnology: "BackEnd",
+  },
+  {
+    name: "HTML",
+    color: "#EF652A",
+    tecnology: "FrontEnd",
+  },
+  {
+    name: "CSS",
+    color: "#30A9DC",
+    tecnology: "FrontEnd",
+  },
+  {
+    name: "JavaScript",
+    color: "#F0DB4F",
+    tecnology: "FrontEnd",
+  },
+  {
+    name: "TypeScript",
+    color: "#3178C6",
+    tecnology: "FrontEnd",
+  },
+  {
+    name: "Tailwind",
+    color: "#17BAB8",
+    tecnology: "FrontEnd",
+  },
+  {
+    name: "React",
+    color: "#00D8FF",
+    tecnology: "FrontEnd",
+  },
+
+  // {
+  //   name: "Next",
+  //   color: "#FAFAFA",
+  // },
+  {
+    name: "Astro",
+    color: "#FAFAFA",
+    tecnology: "FrontEnd",
+  },
+  {
+    name: "NodeJS",
+    color: "#539E43",
+    tecnology: "Learning",
+  },
+  {
+    name: "Spring",
+    color: "#539E43",
+    tecnology: "Learning",
+  },
+  {
+    name: "Python",
+    color: "#3178C6",
+    tecnology: "Learning",
+  },
+
+  {
+    name: "Next",
+    color: "#FAFAFA",
+    tecnology: "Learning",
+  },
+  {
+    name: "IntellijIdea",
+    color: "#F6009C",
+    tecnology: "Herramientas",
+  },
+  {
+    name: "Git",
+    color: "#DE4C36",
+    tecnology: "Herramientas",
+  },
+
+  {
+    name: "VS Code",
+    color: "#3DA9F2",
+    tecnology: "Herramientas",
+  },
+  {
+    name: "NPM",
+    color: "#DC312F",
+    tecnology: "Herramientas",
+  },
+  {
+    name: "Notion",
+    color: "#FAFAFA",
+    tecnology: "Herramientas",
+  },
+  {
+    name: "Terminal",
+    color: "#FAFAFA",
+    tecnology: "Herramientas",
+  },
+];
